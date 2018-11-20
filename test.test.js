2451124b35fab5b5e3f06d07c28a95897eebd656
asherwebb <ashersweb@gmail.com>
2018-11-20 11:09:59 -0500

    re-use nightmare for multiple tests

--- test.test.js
+++ test.test.js
@@ -1,9 +1,15 @@
-var Nightmare = require('nightmare');
-jest.setTimeout(60000)
+var Nightmare = require('nightmare')
 
 describe( 'integration testing', () => {
+  jest.setTimeout('60000')
+
+  let nightmare = null
+
+  beforeEach(() => {
+    nightmare = new Nightmare()
+  })
+
   it( 'should be able to reach home page', done => {
-    var nightmare = new Nightmare();
     nightmare
       .viewport(1500 + Math.floor(Math.random() * 400), 700 + Math.floor(Math.random() * 300))
       .goto('http://staging.usedequipmentguide.com')
@@ -23,3 +29,18 @@ describe( 'integration testing', () => {
       .catch( done )
   })
 })
+
+// .goto('http://staging.usedequipmentguide.com/search')
+// .wait(1000)
+// .wait('#filters-bucket')
+// .evaluate( () => {
+//   return {
+//     url: window.location.href,
+//     html: document.querySelector("[href='/search?category=On-Highway']").innerHTML
+//   }
+// })
+// .end()
+// .then( (result) => {
+//   expect(result.html).toBe("On-Highway Trucks & Trailers")
+//   done()
+// })
